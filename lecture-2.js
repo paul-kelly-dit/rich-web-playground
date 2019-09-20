836afdee58337248a2051a1d1b4b76c4be7a2be2
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2019-09-20 15:49:57 +0100

    Create lecture-2.js

---
+++ lecture-2.js
@@ -0,0 +1,26 @@
+function discountPricesVar (applyDiscount) {
+  var discounted = []
+
+  if (applyDiscount) {
+    var discountedPrice = 20;
+    discounted.push(discountedPrice)
+  }
+  console.log('Discounted Price: ' + discountedPrice);
+  return discounted;
+};
+
+var prices = discountPricesVar(true);
+
+function discountPricesLet (applyDiscount) {
+    let discounted = []
+  
+    if (applyDiscount) {
+      let discountedPrice = 20;
+      discounted.push(discountedPrice)
+    }
+    console.log('Discounted Price: ' + discountedPrice);
+    return discounted;
+  };
+  
+  let pricesLet = discountPricesLet(true);
+  
