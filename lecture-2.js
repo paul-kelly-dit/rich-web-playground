f97bb6583c396ababf9005b26b38ce39309071c8
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2019-09-20 16:16:17 +0100

    Added functions as params

--- lecture-2.js
+++ lecture-2.js
@@ -1,29 +1,64 @@
 // 1. let vs var start
 
 function discountPricesVar (applyDiscount) {
-  var discounted = []
-
-  if (applyDiscount) {
-    var discountedPrice = 20;
-    discounted.push(discountedPrice)
-  }
-  console.log('Discounted Price: ' + discountedPrice);
-  return discounted;
-};
-
-var prices = discountPricesVar(true);
-
-function discountPricesLet (applyDiscount) {
-    let discounted = []
+    var discounted = []
   
     if (applyDiscount) {
-      let discountedPrice = 20;
+      var discountedPrice = 20;
       discounted.push(discountedPrice)
     }
     console.log('Discounted Price: ' + discountedPrice);
     return discounted;
   };
   
-  let pricesLet = discountPricesLet(true);
+  var prices = discountPricesVar(true);
   
-// let vs var finish..
+  function discountPricesLet (applyDiscount) {
+      let discounted = []
+    
+      if (applyDiscount) {
+        let discountedPrice = 20;
+        discounted.push(discountedPrice)
+      }
+      console.log('Discounted Price: ' + discountedPrice);
+      return discounted;
+    };
+    
+    let pricesLet = discountPricesLet(true);
+    
+  // let vs var finish..
+  
+  // 2. Functions as Function Arguments
+  const squared = (n1) => { 
+      let result = n1 * n1;
+      console.log("Result : " + result);
+      return result;
+  }
+
+  const adder = (n1) => { 
+    let result = n1 + 1;
+    console.log("Result : " + result);
+    return result;
+  }
+
+  [1, 2, 3, 4, 5].forEach(squared);​
+  [1, 2, 3, 4, 5].forEach(adder);​
+
+  // As opposed too
+
+let numbers = [1, 2, 3, 4, 5];
+
+for (var i = 0; i < numbers.length; i++) {
+    let result = numbers[i] * numbers[i];
+    console.log(result);
+}  
+
+
+for (var i = 0; i < numbers.length; i++) {
+    let result = numbers[i] + 1;
+    console.log(result);
+}  
+
+// Functions as Function Arguments finish
+  
+  
