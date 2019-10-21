8b19f1f29c551e2640d01927965e1529f555bc9f
Paul Kelly <paul.kelly2@tudublin.ie>
2019-10-21 10:20:39 +0100

    More examples

--- lecture-6-rxjs.js
+++ lecture-6-rxjs.js
@@ -79,6 +79,60 @@ function mergeMapExample() {
     const subscribeThree = exampleThree.subscribe(val => console.log(val));
 }
 
+function partitionExample() {
+    const source = Rx.Observable.from([1,2,3,4,5,6]);
+//first value is true, second false
+    const [evens, odds] = source.partition(val => val % 2 === 0);
+    /*
+      Output:
+      "Even: 2"
+      "Even: 4"
+      "Even: 6"
+      "Odd: 1"
+      "Odd: 3"
+      "Odd: 5"
+    */
+    const subscribe = Rx.Observable.merge(
+        evens
+            .map(val => `Even: ${val}`),
+        odds
+            .map(val => `Odd: ${val}`)
+    ).subscribe(val => console.log(val));
+//if greater than 3 throw
+    const example = source
+        .map(val => {
+            if(val > 3){
+                throw `${val} greater than 3!`
+            }
+            return {success: val};
+        })
+        .catch(val => Rx.Observable.of({error: val}));
+//split on success or error
+    const [success, error] = example.partition(res => res.success)
+    /*
+      Output:
+      "Success! 1"
+      "Success! 2"
+      "Success! 3"
+      "Error! 4 greater than 3!"
+    */
+    const subscribeTwo = Rx.Observable.merge(
+        success.map(val => `Success! ${val.success}`),
+        error.map(val => `Error! ${val.error}`)
+    ).subscribe(val => console.log(val));
+}
+
+function pluckExample() {
+    const source = Rx.Observable.from([
+        {name: 'Joe', age: 30},
+        {name: 'Sarah', age:35}
+    ]);
+    //grab names
+    const example = source.pluck('name');
+    //output: "Joe", "Sarah"
+    const subscribe = example.subscribe(val => console.log(val));
+}
+
 function everyExample() {
 //emit 5 values
     const source = Rx.Observable.of(1,2,3,4,5);
